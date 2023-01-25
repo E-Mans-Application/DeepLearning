eddf1a913c59cdd498b01745f29626733cd49251
Extaupin <magusuranii@gmail.com>
2023-01-25 19:33:50 +0100

    Update main.py
    Training loop, actuellement cassé

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import torch
 import torch.nn as nn
 import torch.functional as F
 import dbload
+import torch.optim as optim
 
 class ForwardPreHook:
     """Hook to apply pruning. See PyTorch doc about `Module`'s "forward pre-hooks" """
@@ -75,6 +76,32 @@ class LeNet(Prunable):
         x = self.fc3(x)
         return x
 
+def training_loop (epochs, net, tuple_dataloader):
+	#cf  https://pytorch.org/tutorials/beginner/blitz/cifar10_tutorial.html
+	criterion = nn.CrossEntropyLoss()
+	optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+	
+	for i in range(epochs):
+		for i, data in enumerate(tuple_dataloader[0], 0):
+			# get the inputs; data is a list of [inputs, labels]
+			inputs, labels = data
+		
+			# zero the parameter gradients
+			optimizer.zero_grad()
+		
+			# forward + backward + optimize
+			outputs = net(inputs)
+			loss = criterion(outputs, labels)
+			loss.backward()
+			optimizer.step()
+			
+		# print statistics
+		running_loss += loss.item()
+		if i % 2000 == 1999:    # print every 2000 mini-batches
+			print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
+		running_loss = 0.0
+
+
 if __name__ == "__main__":
 
     print(next(iter(dbload.load_mnist()[0])))
@@ -102,3 +129,5 @@ if __name__ == "__main__":
     m.prune_(0.7)
     print("Proportion of nulls (pruned) after:")
     test_prune(m)
+    
+    training_loop(2, m, dbload.load_mnist())
