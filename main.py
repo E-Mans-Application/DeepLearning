904dbab2d8120eefbdefef5033b1946ed3eb419b
Extaupin <magusuranii@gmail.com>
2023-01-26 02:34:10 +0100

    Update main.py
    Correction de bug, training loop, verification loop.

--- main.py
+++ main.py
@@ -3,84 +3,93 @@ from typing import Union
 import numpy as np
 import torch
 import torch.nn as nn
-import torch.functional as F
+import torch.nn.functional as F
 import dbload
 import torch.optim as optim
 
 class ForwardPreHook:
-    """Hook to apply pruning. See PyTorch doc about `Module`'s "forward pre-hooks" """
+	"""Hook to apply pruning. See PyTorch doc about `Module`'s "forward pre-hooks" """
 
-    def __call__(self, module: nn.Module, *args) -> None:
-        for param in module.parameters():
-            param.data[param.mask == 0] = 0
+	def __call__(self, module: nn.Module, *args) -> None:
+		for param in module.parameters():
+			param.data[param.mask == 0] = 0
 
 class Prunable(nn.Module):
-    """Base class for all prunable `Module`s.
-    This class works the same way as the base `Module`,
-    with the difference it has a method `prune_` added."""
+	"""Base class for all prunable `Module`s.
+	This class works the same way as the base `Module`,
+	with the difference it has a method `prune_` added."""
 
-    def __init__(self):
-        super().__init__()
-        self._hook = self.register_forward_pre_hook(ForwardPreHook())
+	def __init__(self):
+		super(Prunable, self).__init__()
+		self._hook = self.register_forward_pre_hook(ForwardPreHook())
 
-    def _create_mask(tensor: torch.Tensor) -> None:
-        """Initializes a mask, filled with ones, for the given tensor."""
-        tensor.mask = torch.ones_like(tensor)
+	def _create_mask(tensor: torch.Tensor) -> None:
+		"""Initializes a mask, filled with ones, for the given tensor."""
+		tensor.mask = torch.ones_like(tensor)
 
-    def __setattr__(self, name: str, value: Union[torch.Tensor, nn.Module]) -> None:
-        # Initializes a mask for parameters and sub-modules
-        if isinstance(value, nn.Module):
-            for param in value.parameters():
-                Prunable._create_mask(param)
-        elif isinstance(value, torch.Tensor):
-            Prunable._create_mask(value)
-        return super().__setattr__(name, value)
-        
-    def prune_(self, perc : float) -> None:
-        """Prunes the model (in-place), disabling `perc * 100` percents of
-        the weights.
-        For each parameter, the weights with the lowest absolute value are pruned.
-        `float` is a number between 0 and 1.
-        
-        ### Note:
-        Pruning does not enforce the gradient to be null in the backward step.
-        It is only applied during evaluation."""
-        for param in self.parameters():
-            q = torch.quantile(abs(param), perc).item()
-            param.mask[abs(param) < q] = 0
+	def __setattr__(self, name: str, value: Union[torch.Tensor, nn.Module]) -> None:
+		# Initializes a mask for parameters and sub-modules
+		if isinstance(value, nn.Module):
+			for param in value.parameters():
+				Prunable._create_mask(param)
+		elif isinstance(value, torch.Tensor):
+			Prunable._create_mask(value)
+		return super().__setattr__(name, value)
+	
+	def num_flat_features(self, x):	# https://discuss.pytorch.org/t/understand-nn-module/8416
+		
+		size = x.size()[1:]  # all dimensions except the batch dimension
+		num_features = 1
+		for s in size:
+			num_features *= s
+		return num_features
+	
+	def prune_(self, perc : float) -> None:
+		"""Prunes the model (in-place), disabling `perc * 100` percents of
+		the weights.
+		For each parameter, the weights with the lowest absolute value are pruned.
+		`float` is a number between 0 and 1.
+		
+		### Note:
+		Pruning does not enforce the gradient to be null in the backward step.
+		It is only applied during evaluation."""
+		for param in self.parameters():
+			q = torch.quantile(abs(param), perc).item()
+			param.mask[abs(param) < q] = 0
 
-        # forces to apply hook (allows to immediately reflect the changes
-        # in tests)
-        ForwardPreHook()(self)
+		# forces to apply hook (allows to immediately reflect the changes
+		# in tests)
+		ForwardPreHook()(self)
 
 class LeNet(Prunable):
-    """Prunable LeNet model
-    
-    Inspired from
-    https://github.com/lychengrex/LeNet-5-Implementation-Using-Pytorch/blob/master/LeNet-5%20Implementation%20Using%20Pytorch.ipynb"""
+	"""Prunable LeNet model
+	
+	Inspired from
+	https://github.com/lychengrex/LeNet-5-Implementation-Using-Pytorch/blob/master/LeNet-5%20Implementation%20Using%20Pytorch.ipynb"""
 
-    def __init__(self):
-        super().__init__()
-        self.conv1 = nn.Conv2d(1, 6, 5, padding=2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+	def __init__(self):
+		super(LeNet, self).__init__()
+		self.conv1 = nn.Conv2d(1, 6, 5, padding=2)
+		self.conv2 = nn.Conv2d(6, 16, 5)
+		self.fc1 = nn.Linear(16 * 5 * 5, 120)
+		self.fc2 = nn.Linear(120, 84)
+		self.fc3 = nn.Linear(84, 10)
 
-    def forward(self, x):
-        x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
-        x = F.max_pool2d(F.relu(self.conv2(x)), (2, 2))
-        x = x.view(-1, self.num_flat_features(x))
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
+	def forward(self, x):
+		x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
+		x = F.max_pool2d(F.relu(self.conv2(x)), (2, 2))
+		x = x.view(-1, super().num_flat_features(x))
+		x = F.relu(self.fc1(x))
+		x = F.relu(self.fc2(x))
+		x = self.fc3(x)
+		return x
 
 def training_loop (epochs, net, tuple_dataloader):
-	#cf  https://pytorch.org/tutorials/beginner/blitz/cifar10_tutorial.html
+	#cf	 https://pytorch.org/tutorials/beginner/blitz/cifar10_tutorial.html
 	criterion = nn.CrossEntropyLoss()
 	optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 	
+	running_loss = 0.0
 	for i in range(epochs):
 		for i, data in enumerate(tuple_dataloader[0], 0):
 			# get the inputs; data is a list of [inputs, labels]
@@ -97,37 +106,60 @@ def training_loop (epochs, net, tuple_dataloader):
 			
 		# print statistics
 		running_loss += loss.item()
-		if i % 2000 == 1999:    # print every 2000 mini-batches
-			print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
+		if i % 2000 == 1999:	# print every 2000 mini-batches
+			print(f'[{epochs + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
 		running_loss = 0.0
 
+def validation(net, tuple_dataloader):
+	#cf	 https://pytorch.org/tutorials/beginner/blitz/cifar10_tutorial.html
+	correct = 0
+	total = 0
+	# since we're not training, we don't need to calculate the gradients for our outputs
+	with torch.no_grad():
+		for data in tuple_dataloader[1]:
+			images, labels = data
+			# calculate outputs by running images through the network
+			outputs = net(images)
+			# the class with the highest energy is what we choose as prediction
+			_, predicted = torch.max(outputs.data, 1)
+			total += labels.size(0)
+			correct += (predicted == labels).sum().item()
+
+	print(f'Accuracy of the network on the 10000 test images: {100 * correct // total} %')
 
 if __name__ == "__main__":
+	
+	truc = Prunable()
+	x = torch.tensor([[1., -1.], [1., -1.]])
+	print("test1: ", truc.num_flat_features(x))
+	
+	
+	print(next(iter(dbload.load_mnist()[0])))
+	
+	def test_prune(m: Prunable):
+		
+		z_g = 0
+		c_g = 0
 
-    print(next(iter(dbload.load_mnist()[0])))
+		for name, param in m.named_parameters():
+			z = torch.sum((param == 0).int())
+			c = torch.numel(param)
 
-    def test_prune(m: Prunable):
-            
-        z_g = 0
-        c_g = 0
+			z_g += z
+			c_g += c
 
-        for name, param in m.named_parameters():
-            z = torch.sum((param == 0).int())
-            c = torch.numel(param)
+			print(name, ": ", z / c)
 
-            z_g += z
-            c_g += c
+		print("Global: ", z_g / c_g)
 
-            print(name, ": ", z / c)
+	m = LeNet()
+	print("Proportion of nulls (pruned) before:")
+	test_prune(m)
 
-        print("Global: ", z_g / c_g)
-
-    m = LeNet()
-    print("Proportion of nulls (pruned) before:")
-    test_prune(m)
-
-    m.prune_(0.7)
-    print("Proportion of nulls (pruned) after:")
-    test_prune(m)
-    
-    training_loop(2, m, dbload.load_mnist())
+	m.prune_(0.7)
+	print("Proportion of nulls (pruned) after:")
+	test_prune(m)
+	
+	mist_db = dbload.load_mnist()
+	training_loop(2, m, mist_db)
+	validation(m, mist_db)
